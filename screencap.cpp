0a9c978b5f141e74a82d3f7a618812316860ad2e
Joseph Pollack <jpollack320@gmail.com>
2016-07-18 00:50:32 -0400

    antialias working

--- screencap.cpp
+++ screencap.cpp
@@ -30,27 +30,25 @@ bool saveImage(float image_scale, const fractal_generator &fg, const shared_ptr<
 	GLsizei width(image_width);
 	GLsizei height(image_height);
 
-	// create a framebuffer
-	GLuint fbo_id;
-	glGenFramebuffersEXT(1, &fbo_id);
-	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo_id);
+	glEnable(GL_MULTISAMPLE);
 
-	// create a multisample color buffer
-	GLuint color_buffer;
-	glGenRenderbuffersEXT(1, &color_buffer);
-	glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, color_buffer);
-	signed int samples = 4;
-	glRenderbufferStorageMultisample(GL_RENDERBUFFER_EXT, samples, GL_RGBA8, width, height);
+	// initialize multisample texture
+	GLuint multisample_tex;
+	glGenTextures(1, &multisample_tex);
+	glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, multisample_tex);
+	glTexParameteri(GL_TEXTURE_2D_MULTISAMPLE, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D_MULTISAMPLE, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D_MULTISAMPLE, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D_MULTISAMPLE, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	//NULL means reserve texture memory, but texels are undefined
+	glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, 4, GL_RGBA8, width, height, GL_FALSE);
 
-	// create a multisample depth buffer
-	GLuint depth_buffer;
-	glGenRenderbuffersEXT(1, &depth_buffer);
-	glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, depth_buffer);
-	glRenderbufferStorageMultisampleEXT(GL_RENDERBUFFER_EXT, samples, GL_DEPTH_COMPONENT24, width, height);
-
-	// attach renderbuffers to FBO
-	glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT, GL_RENDERBUFFER_EXT, color_buffer);
-	glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, GL_DEPTH_ATTACHMENT_EXT, GL_RENDERBUFFER_EXT, depth_buffer);
+	// initialize multisample fbo
+	GLuint multisample_fbo;
+	glGenFramebuffers(1, &multisample_fbo);
+	glBindFramebuffer(GL_FRAMEBUFFER, multisample_fbo);
+	// attach multisample texture
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, multisample_tex, 0);
 
 	char error_messages[256];
 	GLint value = glExtCheckFramebufferStatus(error_messages);
@@ -60,24 +58,59 @@ bool saveImage(float image_scale, const fractal_generator &fg, const shared_ptr<
 		return false;
 	}
 
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glViewport(0, 0, width, height);
+	
+	//glMatrixMode(GL_PROJECTION);
+	//glLoadIdentity();
+	//glOrtho(0.0, (GLdouble)width, 0.0, (GLdouble)height, -1.0, 1.0);
+
 	// render
 	glUniform1f(context->getShaderGLint("point_size_scale"), image_scale);
 	fg.drawFractal();
 	glUniform1f(context->getShaderGLint("point_size_scale"), 1.0f);
 
-	// copy to multisampled fbo
-	GLuint multisample_fbo_id;
-	glGenFramebuffersEXT(1, &multisample_fbo_id);
-	glBindFramebufferEXT(GL_READ_FRAMEBUFFER_EXT, multisample_fbo_id);
-	glBindFramebufferEXT(GL_DRAW_FRAMEBUFFER_EXT, fbo_id);
-	glBlitFramebufferEXT(0, 0, width, height, 0, 0, width, height, GL_COLOR_BUFFER_BIT, GL_NEAREST);
+	// initialize downsample texture
+	GLuint downsample_tex;
+	glGenTextures(1, &downsample_tex);
+	glBindTexture(GL_TEXTURE_2D, downsample_tex);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	//NULL means reserve texture memory, but texels are undefined
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_BGRA, GL_UNSIGNED_BYTE, NULL);
 
-	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, fbo_id);
+	// initialize downsample fbo
+	GLuint downsample_fbo;
+	glGenFramebuffers(1, &downsample_fbo);
+	glBindFramebuffer(GL_FRAMEBUFFER, downsample_fbo);
+	// attach downsample texture
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, downsample_tex, 0);
+
+	value = glExtCheckFramebufferStatus(error_messages);
+	if (value < 0)
+	{
+		cout << error_messages << endl;
+		return false;
+	}
+
+	// select multisample framebuffer for reading
+	glBindFramebuffer(GL_READ_FRAMEBUFFER, multisample_fbo);
+	// select downsample framebuffer for drawing
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, downsample_fbo);
+	
+	// blit from read framebuffer to draw framebuffer
+	glBlitFramebuffer(0, 0, width, height, 0, 0, width, height, GL_COLOR_BUFFER_BIT, GL_NEAREST);
 
 	GLubyte *pixels = new GLubyte[width * height * 4];
 	if (pixels == NULL)
 		return false;
 
+	// select downsample framebuffer for reading
+	glBindFramebuffer(GL_READ_FRAMEBUFFER, downsample_fbo);
+
+	// read pixels
 	glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
 
 	string filename;
@@ -114,11 +147,8 @@ bool saveImage(float image_scale, const fractal_generator &fg, const shared_ptr<
 	delete[] pixels;
 
 	// delete resources
-	glDeleteRenderbuffersEXT(1, &color_buffer);
-	glDeleteRenderbuffersEXT(1, &depth_buffer);
-	glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, 0);
-	glDeleteFramebuffersEXT(1, &fbo_id);
-	glDeleteFramebuffersEXT(1, &multisample_fbo_id);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glDeleteFramebuffers(1, &multisample_fbo);
 
 	context->setBackgroundColor(background_color);
 
@@ -134,35 +164,29 @@ bool saveImage(float image_scale, const fractal_generator &fg, const shared_ptr<
 GLint glExtCheckFramebufferStatus(char *error_message)
 {
 	GLenum status;
-	status = glCheckFramebufferStatusEXT(GL_FRAMEBUFFER_EXT);
+	status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
 	switch (status)
 	{
-	case GL_FRAMEBUFFER_COMPLETE_EXT:
+	case GL_FRAMEBUFFER_COMPLETE:
 		break;
-	case GL_FRAMEBUFFER_UNSUPPORTED_EXT:
+	case GL_FRAMEBUFFER_UNSUPPORTED:
 		//Choose different formats
-		strcpy(error_message, "Framebuffer object format is unsupported by the video hardware. (GL_FRAMEBUFFER_UNSUPPORTED_EXT)(FBO - 820)");
+		strcpy(error_message, "Framebuffer object format is unsupported by the video hardware. (GL_FRAMEBUFFER_UNSUPPORTED)(FBO - 820)");
 		return -1;
-	case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT_EXT:
-		strcpy(error_message, "Incomplete attachment. (GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT_EXT)(FBO - 820)");
+	case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
+		strcpy(error_message, "Incomplete attachment. (GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT)(FBO - 820)");
 		return -1;
-	case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT_EXT:
-		strcpy(error_message, "Incomplete missing attachment. (GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT_EXT)(FBO - 820)");
+	case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
+		strcpy(error_message, "Incomplete missing attachment. (GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT)(FBO - 820)");
 		return -1;
-	case GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS_EXT:
-		strcpy(error_message, "Incomplete dimensions. (GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS_EXT)(FBO - 820)");
+	case GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER:
+		strcpy(error_message, "Incomplete draw buffer. (GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER)(FBO - 820)");
 		return -1;
-	case GL_FRAMEBUFFER_INCOMPLETE_FORMATS_EXT:
-		strcpy(error_message, "Incomplete formats. (GL_FRAMEBUFFER_INCOMPLETE_FORMATS_EXT)(FBO - 820)");
+	case GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER:
+		strcpy(error_message, "Incomplete read buffer. (GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER)(FBO - 820)");
 		return -1;
-	case GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER_EXT:
-		strcpy(error_message, "Incomplete draw buffer. (GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER_EXT)(FBO - 820)");
-		return -1;
-	case GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER_EXT:
-		strcpy(error_message, "Incomplete read buffer. (GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER_EXT)(FBO - 820)");
-		return -1;
-	case GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE_EXT:
-		strcpy(error_message, "Incomplete multisample buffer. (GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE_EXT)(FBO - 820)");
+	case GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE:
+		strcpy(error_message, "Incomplete multisample buffer. (GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE)(FBO - 820)");
 		return -1;
 	default:
 		//Programming error; will fail on all hardware
